8a717f4ee1bd8ecd1db4a2570aa80ef087399b7e
RoneyJ <jpr87@case.edu>
2018-11-05 16:31:30 -0500

    update

--- Josh_Roney_HW2/16_bit_adder.v
+++ Josh_Roney_HW2/16_bit_adder.v
@@ -5,8 +5,8 @@ module adder(
 	output [15:0] C;
 	output vout, cout; //signed overflow and carry output
 );
-reg [15:0]a,b,c;
-reg i,oe,vo,co;
+reg [15:0]c;
+reg vo,co;
 wire [14:0] carry;
 
 
@@ -14,59 +14,61 @@ wire [14:0] carry;
 case(CODE)
 	3'b000: //signed addition
 	begin
-		c[0] = a[0] ^ b[0] ^ cin;
-		carry[0] = (a[0]&b[0]) | (a[0]&cin) | (b[0]&cin);
+		c[0] = A[0] ^ B[0] ^ cin;
+		carry[0] = (A[0]&B[0]) | (A[0]&cin) | (B[0]&cin);
 		
-		c[1] = a[1] ^ b[1] ^ carry[0];
-		carry[1] = (a[1]&b[1]) | (a[1]&carry[0]) | (b[1]&carry[0]);
+		c[1] = A[1] ^ B[1] ^ carry[0];
+		carry[1] = (A[1]&B[1]) | (A[1]&carry[0]) | (B[1]&carry[0]);
 		
-		c[2] = a[2] ^ b[2] ^ carry[1];
-		carry[2] = (a[2]&b[2]) | (a[2]&carry[1]) | (b[2]&carry[1]);
+		c[2] = A[2] ^ B[2] ^ carry[1];
+		carry[2] = (A[2]&B[2]) | (A[2]&carry[1]) | (B[2]&carry[1]);
 		
-		c[1] = a[1] ^ b[1] ^ carry[0];
-		carry[3] = (a[3]&b[3]) | (a[3]&carry[2]) | (b[3]&carry[2]);
+		c[1] = A[1] ^ B[1] ^ carry[0];
+		carry[3] = (A[3]&B[3]) | (A[3]&carry[2]) | (B[3]&carry[2]);
 		
-		c[4] = a[4] ^ b[4] ^ carry[3];
-		carry[4] = (a[4]&b[4]) | (a[4]&carry[3]) | (b[4]&carry[3]);
+		c[4] = A[4] ^ B[4] ^ carry[3];
+		carry[4] = (A[4]&B[4]) | (A[4]&carry[3]) | (B[4]&carry[3]);
 		
-		c[5] = a[5] ^ b[5] ^ carry[4];
-		carry[5] = (a[5]&b[5]) | (a[5]&carry[4]) | (b[5]&carry[4]);
+		c[5] = A[5] ^ B[5] ^ carry[4];
+		carry[5] = (A[5]&B[5]) | (A[5]&carry[4]) | (B[5]&carry[4]);
 		
-		c[6] = a[6] ^ b[6] ^ carry[5];
-		carry[6] = (a[6]&b[6]) | (a[6]&carry[5]) | (b[6]&carry[5]);
+		c[6] = A[6] ^ B[6] ^ carry[5];
+		carry[6] = (A[6]&B[6]) | (A[6]&carry[5]) | (B[6]&carry[5]);
 		
-		c[7] = a[7] ^ b[7] ^ carry[6];
-		carry[7] = (a[7]&b[7]) | (a[7]&carry[6]) | (b[7]&carry[6]);
+		c[7] = A[7] ^ B[7] ^ carry[6];
+		carry[7] = (A[7]&B[7]) | (A[7]&carry[6]) | (B[7]&carry[6]);
 		
-		c[8] = a[8] ^ b[8] ^ carry[7];
-		carry[8] = (a[8]&b[8]) | (a[8]&carry[7]) | (b[8]&carry[7]);
+		c[8] = A[8] ^ B[8] ^ carry[7];
+		carry[8] = (A[8]&B[8]) | (A[8]&carry[7]) | (B[8]&carry[7]);
 		
-		c[9] = a[9] ^ b[9] ^ carry[8];
-		carry[9] = (a[9]&b[9]) | (a[9]&carry[8]) | (b[9]&carry[8]);
+		c[9] = A[9] ^ B[9] ^ carry[8];
+		carry[9] = (A[9]&B[9]) | (A[9]&carry[8]) | (B[9]&carry[8]);
 		
-		c[10] = a[10] ^ b[10] ^ carry[9];
-		carry[10] = (a[10]&b[10]) | (a[10]&carry[9]) | (b[10]&carry[9]);
+		c[10] = A[10] ^ B[10] ^ carry[9];
+		carry[10] = (A[10]&B[10]) | (A[10]&carry[9]) | (B[10]&carry[9]);
 		
-		c[11] = a[11] ^ b[11] ^ carry[10];
-		carry[11] = (a[11]&b[11]) | (a[11]&carry[10]) | (b[11]&carry[10]);
+		c[11] = A[11] ^ B[11] ^ carry[10];
+		carry[11] = (A[11]&B[11]) | (A[11]&carry[10]) | (B[11]&carry[10]);
 		
-		c[12] = a[12] ^ b[12] ^ carry[11];
-		carry[12] = (a[12]&b[12]) | (a[12]&carry[11]) | (b[12]&carry[11]);
+		c[12] = A[12] ^ B[12] ^ carry[11];
+		carry[12] = (A[12]&B[12]) | (A[12]&carry[11]) | (B[12]&carry[11]);
 		
-		c[13] = a[13] ^ b[13] ^ carry[12];
-		carry[13] = (a[13]&b[13]) | (a[13]&carry[12]) | (b[13]&carry[12
+		c[13] = A[13] ^ B[13] ^ carry[12];
+		carry[13] = (A[13]&B[13]) | (A[13]&carry[12]) | (B[13]&carry[12
 		
-		c[14] = a[14] ^ b[14] ^ carry[13];
-		carry[14] = (a[14]&b[14]) | (a[13]&carry[13]) | (b[13]&carry[13]);
+		c[14] = A[14] ^ B[14] ^ carry[13];
+		carry[14] = (A[14]&B[14]) | (A[13]&carry[13]) | (B[13]&carry[13]);
 		
-		c[15] = a[15] ^ b[15] ^ carry[14];
-		co = (a[15]&b[15]) | (a[15]&carry[14]) | (b[15]&carry[14]);
+		c[15] = A[15] ^ B[15] ^ carry[14];
+		co = (A[15]&B[15]) | (A[15]&carry[14]) | (B[15]&carry[14]);
 
-		if(a[15]&b[15] == c[15])
-			vo = 0;
-		else
-			vo = 1;
-	
+		if(~coe)
+		begin
+			if(A[15]&B[15] == c[15])
+				vo = 0;
+			else
+				vo = 1;
+		end
 	end
 
 	3'b001: //unsigned addition
@@ -92,5 +94,5 @@ endcase
 
 assign C = c;
 assign cout = co;
-assign vo = cout;
+assign vout = vo;
 endmodule 
