41f8fee4228cd34a05cc7896ea3315f6849cfb18
RoneyJ <jpr87@case.edu>
2018-11-29 13:58:04 -0500

    processore update

--- Josh_Roney_HW3/processor.v
+++ Josh_Roney_HW3/processor.v
@@ -1,89 +1,136 @@
 //Processor module for HW3
 //Josh Roney (jpr87)
-
-`include "ALU.v"
-
-module processor(data, clk, out_data);
-input [31:0] data;
+module processor(clk);
 input clk;
-output [31:0] out_data;
 
-reg [3:0] opcode;		//opcode
-reg [3:0] cc;			//condition code
-reg src_type,dest_type;		//source/destination type
-reg [11:0] src_addr;		//source address
-reg [11:0] count;		//shift/rotate count
-reg [11:0] dest_addr;		//destination address
+reg fetch, decode, execute, writeback;	//assertions to determine which task to perform at clock
 
-reg [31:0] memory [0:15];	//memory of 16 32-bit registers, the first 12 bits are the address field
+reg [3:0] opcode;			//opcode
+reg [3:0] cc;				//condition code
+reg src_type,dest_type;	//source/destination type
+reg [11:0] src_addr;		//source address or shift/rotate amount
+reg [11:0] dest_addr;	//destination address
 
-reg [15:0] A,B,C;
-reg [4:0] code;
-reg overlfow;
+integer PC;					//program counter
+reg [31:0] instr;			//instruction storage
+reg [31:0] src_data;		//data stored at source address
+reg [31:0] dest_data;	//data stored at destination address
 
-ALU alu (A,B,code,C,overflow);
+reg [4:0] PSR;				//Program Status Register
+reg [31:0] mem [0:5];	//memory used by processor, first X registers are data
+
+reg [31:0] regfile;	//Register File
+
+initial
+begin
+	$monitor("Mem[0] = %d \t Mem[1] = %d \t Mem[2] = %h",mem[0],mem[1],mem[2]);
+	fetch = 1;
+	decode = 0;
+	execute = 0;
+	writeback = 0;
+	PSR = 5'b00000;
+	PC = 2;//4;
+	
+	mem[0] = 3;
+	mem[1] = 4;
+	mem[2] = 32'h01010000000000000000000000000001;
+end
 
 always @(posedge clk)
 begin
-	opcode = address[31:28];
-	cc = address[27:24];
-	src_type = address[27];
-	dest_type = address[26];
-	src_addr = address[23:12];
-	count = address[23:12];
-	dest_addr = address[11:0];
+	if(fetch)			//fetch instruction from memory
+	begin
+		instr = mem[PC];
+		
+		fetch = 0;
+		decode = 1;
+	end
 	
+	else if(decode)	//decode instruction
+	begin
+		opcode = instr[31:28];
+		cc = instr[27:24];
+		src_type = instr[27];
+		dest_type = instr[26];
+		src_addr = instr[23:12];
+		dest_addr = instr[11:0];
+		
+		decode = 0;
+		execute = 1;
+	end
+	
+	else if(execute)	//execute instruction depending on opcode
+	begin
 	case(opcode)
-		4'b0000:	//NOP
+		4'b0000:	//No operation
 		begin
-			
+			PC = PC + 1;
+		
+			fetch = 1;
+			execute = 0;
 		end
-
+	
 		4'b0001:	//Load
 		begin
-			
 		end
-
+	
 		4'b0010:	//Store
 		begin
-			
 		end
-
+	
 		4'b0011:	//Branch
 		begin
-			
 		end
-
+	
 		4'b0100:	//XOR
 		begin
-			
 		end
-
+	
 		4'b0101:	//ADD
 		begin
-			
+			//set PSR
+			src_data = mem[src_addr];
+			dest_data = mem[dest_addr];
+		
+			dest_data = dest_data + src_data;
+		
+			PC = PC + 1;
+			execute = 0;
+			writeback = 1;
 		end
-
+	
 		4'b0110:	//Rotate
 		begin
-			
 		end
-
+	
 		4'b0111:	//Shift
 		begin
-			
 		end
-
+	
 		4'b1000:	//Halt
 		begin
+			PC = PC + 1;
 			
+			execute = 0;
+			fetch = 1;
 		end
-
+		
 		4'b1001:	//Complement
 		begin
 			
 		end
-
-	endcase
+	
+	execute = 0;
+	writeback = 1;
+	end
+	
+	else if(writeback)
+	begin
+		mem[dest_addr] = dest_data;
+	
+		writeback = 0;
+		fetch = 1;
+	end
+	
 end
 endmodule
